5cb0397f5d1228bd5f8c32d0f690adb8522e703b
Nicolò Pincelli <pincelli.n@gmail.com>
2025-07-03 17:55:58 +0200

    fix: router page and create 404 page not found

--- router.js
+++ router.js
@@ -5,15 +5,23 @@ function handleRequest(req, res) {
   const method = req.method;
 
   if (url === "/") {
+    res.setHeader("Content-Type", "text/html; charset=UTF-8");
+    res.write("<!DOCTYPE html>");
     res.write("<html>");
+    res.write("<head>");
     res.write("<meta charset='UTF-8'>");
-    res.write("<head><title> First Router Alone </title><head>");
-    res.write("<body> ");
-    res.write("<h1> First Router Node.js</h1> ");
+    res.write("<title> First Router Alone </title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<h1> First Router Node.js</h1>");
     res.write(
-      "<form action='/create-users' method='POST'> <label for='username'> UserName</laber> <input type='text' name='username'> <button type='submit'>Create</button> </form>"
+      "<form action='/create-users' method='POST'> " +
+        "<label for='username'>UserName</label> " +
+        "<input type='text' name='username'> " +
+        "<button type='submit'>Create</button> " +
+        "</form>"
     );
-    res.write(" </form>");
+    res.write("</body>");
     res.write("</html>");
     return res.end();
   }
@@ -21,26 +29,37 @@ function handleRequest(req, res) {
   if (url === "/create-users" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
-      console.log(chunk);
       body.push(chunk);
     });
     req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      console.log(message);
-      return res.end();
+      console.log("User creato:", decodeURIComponent(message)); // decodifica caratteri tipo %20
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      res.end();
     });
   }
 
-  res.setHeader("Content-Type", "text/html; charset=UTF-8");
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html; charset=UTF-8");
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<meta charset='UTF-8'>");
+    res.write("<title> First Router Alone </title>");
+    res.write("</head>");
+    res.write(
+      "<body><ul> <li>Nicolò Pincelli</li> <li>Marika Keller</li> <li>Evelyn Pincelli</li> <li>Gino Pino</li> </ul> </body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
-  res.write("<html>");
-  res.write("<meta charset='UTF-8'>");
-  res.write("<head><title> First Router Alone </title><head>");
-  res.write(
-    "<body><ul> <li>Nicolò Pincelli</li> <li>Marika Keller</li> <li>Evelyn Pincelli</li> <li>Gino Pino</li> </ul> </body>"
-  );
-  res.write("</html>");
+  // Rotta non trovata
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/html; charset=UTF-8");
+  res.write("<html><body><h1>404 Not Found</h1></body></html>");
   res.end();
 }
 
